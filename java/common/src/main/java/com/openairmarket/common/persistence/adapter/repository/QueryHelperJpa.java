9fe0035536f94dae0150ea9285787f9a94c51b8f
Edgar Rico <edgar.martinez.rico@gmail.com>
2018-06-21 19:31:01 -0700

    Adding QueryHelperJpa.

---
+++ java/common/src/main/java/com/openairmarket/common/persistence/adapter/repository/QueryHelperJpa.java
@@ -0,0 +1,108 @@
+package com.openairmarket.common.persistence.adapter.repository;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.util.List;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+/**
+ * The QueryHelperJpa interface defines functionality to perform queries to the persistence layer.
+ *
+ * @param <CQ> the type of the {@code CriteriaQuery}.
+ * @param <R> the type of the {@code Root}.
+ */
+public final class QueryHelperJpa<CQ, R> {
+
+  private final EntityManager entityManager;
+  private final CriteriaBuilder criteriaBuilder;
+  private final CriteriaQuery<CQ> criteriaQuery;
+  private final Root<R> root;
+
+  @Inject
+  public QueryHelperJpa(EntityManager entityManager, Class<CQ> result, Class<R> from) {
+    assertArgumentNotNull(entityManager, "Entity Manager cannot be null.");
+    assertArgumentNotNull(
+        entityManager.getCriteriaBuilder(), "The Criteria Builder cannot be null.");
+    assertArgumentNotNull(result, "The expected outcome result class cannot be null.");
+    this.entityManager = entityManager;
+    this.criteriaBuilder = getEntityManager().getCriteriaBuilder();
+    this.criteriaQuery = criteriaBuilder.createQuery(result);
+    this.root = criteriaQuery.from(from);
+    assertArgumentNotNull(criteriaQuery, "The Criteria Query cannot be null.");
+    assertArgumentNotNull(root, "The Root cannot be null.");
+  }
+
+  public EntityManager getEntityManager() {
+    return entityManager;
+  }
+
+  public CriteriaBuilder getBuilder() {
+    return criteriaBuilder;
+  }
+
+  public CriteriaQuery<CQ> getQuery() {
+    return criteriaQuery;
+  }
+
+  public Root<R> getRoot() {
+    return root;
+  }
+
+  public CQ getSingleResult() {
+    return createTypedQuery().getSingleResult();
+  }
+
+  public List<CQ> getResultList() {
+    return createTypedQuery().getResultList();
+  }
+
+  public List<CQ> getResultList(int firstResult, int maxResults) {
+    TypedQuery<CQ> typedQuery = createTypedQuery();
+    typedQuery.setFirstResult(firstResult);
+    typedQuery.setMaxResults(maxResults);
+    return typedQuery.getResultList();
+  }
+
+  private TypedQuery<CQ> createTypedQuery() {
+    return getEntityManager().createQuery(criteriaQuery);
+  }
+
+  /**
+   * Create a new count query.
+   *
+   * @param <T> the type that will be use to create the {@code QueryContainer}.
+   * @param entityManager the instance that will be use to create the {@code CriteriaQuery}.
+   * @param clase the class that will be use to create the {@code CriteriaQuery} and the {@code
+   *     Root}.
+   * @return new instance
+   */
+  public static final <T> QueryHelperJpa<Long, T> newQueryCounter(
+      EntityManager entityManager, Class<T> clase) {
+    QueryHelperJpa<Long, T> qc = new QueryHelperJpa<>(entityManager, Long.class, clase);
+    qc.getQuery().select(qc.getBuilder().countDistinct(qc.getRoot()));
+    return qc;
+  }
+
+  /**
+   * Creates a new instance of {@link QueryHelperJpa}.
+   *
+   * @param <T> the type that will be use to create the {@code QueryContainer}.
+   * @param entityManager the instance that will be use to create the {@code CriteriaQuery}.
+   * @param clase the class that will be use to create the {@code CriteriaQuery} and the {@code
+   *     Root}.
+   * @return new instance
+   */
+  public static final <T> QueryHelperJpa<T, T> newQuery(
+      EntityManager entityManager, Class<T> clase) {
+    return new QueryHelperJpa<>(entityManager, clase, clase);
+  }
+
+  private static final void assertArgumentNotNull(Object anObject, String aMessage) {
+    checkNotNull(anObject, aMessage);
+  }
+}
--- java/common/src/main/java/com/openairmarket/common/persistence/domain/repository/QueryHelper.java
+++
@@ -1,4 +0,0 @@
-package com.openairmarket.common.persistence.domain.repository;
-
-/** The QueryHelper interface defines functionality to perform queries to the persistence layer. */
-public interface QueryHelper {}
