412d0bb976dd914969a4bd641f6eec6fc4f3f074
encomp <edgar.martinez.rico@gmail.com>
2018-10-21 15:54:11 -0700

    Adding rol to the pipeline

---
+++ java/etl/src/main/pipeline/script/extraction/extract_rol.sql
@@ -0,0 +1,3 @@
+SELECT intIDRol, vcDescRol, vcOperacion, dteFechaOperacion, intIDEmpleado1, bitActivo,
+       bitModificable
+FROM dbo.ROL;
---
+++ java/etl/src/main/pipeline/script/input/input_rol.sql
@@ -0,0 +1,8 @@
+-- Load role for all employees from ms-sql file
+
+DROP TABLE rol IF EXISTS;
+
+CREATE TABLE rol AS SELECT * FROM CSVREAD(@path_input ||
+  'rol.csv', NULL,'charset=UTF-8 fieldSeparator=;');
+
+CREATE INDEX IF NOT EXISTS rolIndex ON rol(intIDRol);
