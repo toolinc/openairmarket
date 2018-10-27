0bb184cc3912556f83f9fe78dd5067204f9ed2bb
encomp <edgar.martinez.rico@gmail.com>
2018-10-26 18:54:01 -0700

    Refactoring the Price list

---
+++ java/pos-persistence/src/main/java/com/openairmarket/pos/persistence/model/audit/price/PriceListAudit.java
@@ -0,0 +1,115 @@
+package com.openairmarket.pos.persistence.model.audit.price;
+
+import com.google.common.base.Preconditions;
+import com.openairmarket.common.persistence.model.audit.AbstractAuditActiveReferenceTenantModel;
+import com.openairmarket.common.persistence.model.location.Currency;
+import com.openairmarket.pos.persistence.model.business.Organization;
+import com.openairmarket.pos.persistence.model.price.PriceList;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import org.eclipse.persistence.annotations.UuidGenerator;
+
+/** Define the revision for the {@link PriceList} entities. */
+@Entity
+@Table(
+    name = "priceListAudit",
+    uniqueConstraints = {
+      @UniqueConstraint(
+          name = "priceListAuditUK",
+          columnNames = {"idPriceList", "createDate"})
+    })
+@UuidGenerator(name = "priceListAudit_gen")
+public final class PriceListAudit extends AbstractAuditActiveReferenceTenantModel {
+
+  @Id
+  @Column(name = "idPriceListAudit")
+  @GeneratedValue(generator = "priceListAudit_gen")
+  private String id;
+
+  @JoinColumn(name = "idPriceList", referencedColumnName = "idPriceList", nullable = false)
+  @ManyToOne(cascade = CascadeType.REFRESH)
+  private PriceList priceList;
+
+  @JoinColumn(name = "idOrganization", referencedColumnName = "idOrganization", nullable = false)
+  @ManyToOne(fetch = FetchType.LAZY)
+  private Organization organization;
+
+  @Column(name = "description", length = 255)
+  private String description;
+
+  @JoinColumn(name = "idCurrency", referencedColumnName = "idCurrency", nullable = false)
+  @ManyToOne(fetch = FetchType.LAZY)
+  private Currency currency;
+
+  @Column(name = "isDefault", nullable = false)
+  private Boolean defaulted;
+
+  @Column(name = "isTaxIncluded", nullable = false)
+  private Boolean taxIncluded;
+
+  @Override
+  public String getId() {
+    return id;
+  }
+
+  @Override
+  public void setId(String id) {
+    this.id = checkNotEmpty(id);
+  }
+
+  public PriceList getPriceList() {
+    return priceList;
+  }
+
+  public void setPriceList(PriceList priceList) {
+    this.priceList = Preconditions.checkNotNull(priceList);
+  }
+
+  public Organization getOrganization() {
+    return organization;
+  }
+
+  public void setOrganization(Organization organization) {
+    this.organization = Preconditions.checkNotNull(organization);
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = checkNotEmpty(description);
+  }
+
+  public Currency getCurrency() {
+    return currency;
+  }
+
+  public void setCurrency(Currency currency) {
+    this.currency = Preconditions.checkNotNull(currency);
+  }
+
+  public Boolean getDefaulted() {
+    return defaulted;
+  }
+
+  public void setDefaulted(Boolean defaulted) {
+    this.defaulted = Preconditions.checkNotNull(defaulted);
+  }
+
+  public Boolean getTaxIncluded() {
+    return taxIncluded;
+  }
+
+  public void setTaxIncluded(Boolean taxIncluded) {
+    this.taxIncluded = Preconditions.checkNotNull(taxIncluded);
+  }
+}
---
+++ java/pos-persistence/src/main/java/com/openairmarket/pos/persistence/model/audit/price/PriceListVersionAudit.java
@@ -0,0 +1,112 @@
+package com.openairmarket.pos.persistence.model.audit.price;
+
+import com.google.common.base.Preconditions;
+import com.openairmarket.common.persistence.model.audit.AbstractAuditActiveReferenceTenantModel;
+import com.openairmarket.pos.persistence.model.price.PriceListSchema;
+import com.openairmarket.pos.persistence.model.price.PriceListVersion;
+import java.util.Date;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.UniqueConstraint;
+import org.eclipse.persistence.annotations.UuidGenerator;
+
+/** Define the revision for the {@link PriceListVersion} entities. */
+@Entity
+@Table(
+    name = "priceListVersionAudit",
+    uniqueConstraints = {
+      @UniqueConstraint(
+          name = "priceListVersionAuditUK",
+          columnNames = {"idPriceListVersion", "createDate"})
+    })
+@UuidGenerator(name = "priceListVersionAudit _gen")
+public final class PriceListVersionAudit extends AbstractAuditActiveReferenceTenantModel {
+
+  @Id
+  @Column(name = "idPriceListVersionAudit")
+  @GeneratedValue(generator = "priceListVersionAudit _gen")
+  private String id;
+
+  @JoinColumn(
+      name = "idPriceListVersion",
+      referencedColumnName = "idPriceListVersion",
+      nullable = false)
+  @ManyToOne(cascade = CascadeType.REFRESH)
+  private PriceListVersion priceListVersion;
+
+  @JoinColumn(
+      name = "idPriceListSchema",
+      referencedColumnName = "idPriceListSchema",
+      nullable = false)
+  @ManyToOne(fetch = FetchType.LAZY)
+  private PriceListSchema priceListSchema;
+
+  @Column(name = "seqNo", nullable = false)
+  private Integer seqNo;
+
+  @Temporal(TemporalType.DATE)
+  @Column(name = "validFrom", nullable = false)
+  private Date validFrom;
+
+  @Column(name = "description", length = 255)
+  private String description;
+
+  @Override
+  public String getId() {
+    return id;
+  }
+
+  @Override
+  public void setId(String id) {
+    this.id = checkNotEmpty(id);
+  }
+
+  public PriceListVersion getPriceListVersion() {
+    return priceListVersion;
+  }
+
+  public void setPriceListVersion(PriceListVersion priceListVersion) {
+    this.priceListVersion = Preconditions.checkNotNull(priceListVersion);
+  }
+
+  public PriceListSchema getPriceListSchema() {
+    return priceListSchema;
+  }
+
+  public void setPriceListSchema(PriceListSchema priceListSchema) {
+    this.priceListSchema = Preconditions.checkNotNull(priceListSchema);
+  }
+
+  public Integer getSeqNo() {
+    return seqNo;
+  }
+
+  public void setSeqNo(Integer seqNo) {
+    this.seqNo = checkPositive(seqNo);
+  }
+
+  public Date getValidFrom() {
+    return validFrom;
+  }
+
+  public void setValidFrom(Date validFrom) {
+    this.validFrom = Preconditions.checkNotNull(validFrom);
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = checkNotEmpty(description);
+  }
+}
