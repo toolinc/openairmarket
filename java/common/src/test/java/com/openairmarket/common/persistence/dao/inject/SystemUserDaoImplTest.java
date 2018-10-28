a43ccff49096f2b14bb2e540bac58d43a02df556
encomp <edgar.martinez.rico@gmail.com>
2018-10-27 22:24:24 -0700

    Refactoring the SystemUserDao tests

--- java/common/src/test/java/com/openairmarket/common/persistence/dao/inject/SystemUserDaoImplTest.java
+++ java/common/src/test/java/com/openairmarket/common/persistence/dao/inject/SystemUserDaoImplTest.java
@@ -12,6 +12,7 @@ import com.openairmarket.common.persistence.dao.security.SystemUserDao;
 import com.openairmarket.common.persistence.inject.DdlGeneration;
 import com.openairmarket.common.persistence.inject.PersistenceModule;
 import com.openairmarket.common.persistence.model.security.SystemUser;
+import java.util.Optional;
 import javax.inject.Provider;
 import javax.persistence.EntityManager;
 import org.junit.jupiter.api.AfterAll;
@@ -65,18 +66,57 @@ public final class SystemUserDaoImplTest {
   }
 
   @Test
-  public void shouldCountInactive() throws DaoException {
-    transactionalObject.get().insertInactive();
+  public void shouldMerge() throws DaoException {
+    String email = "merger@gmail.com".toUpperCase();
+    transactionalObject.get().insert();
+    Optional<SystemUser> systemUser = systemUserDao.get().find(222L);
+    assertThat(systemUser.isPresent()).isTrue();
+    systemUser.get().setEmail(email);
+    entityManager.get().getTransaction().begin();
+    SystemUser userUpdated = systemUserDao.get().merge(systemUser.get());
+    systemUserDao.get().flush();
+    entityManager.get().getTransaction().commit();
+    assertThat(userUpdated.getEmail()).isEqualTo(email);
+  }
+
+  @Test
+  public void shouldCount() {
+    transactionalObject.get().insertCountActive();
+    long counter = systemUserDao.get().count();
+    assertThat(counter).isGreaterThan(0L);
+  }
+
+  @Test
+  public void shouldCountInactive() {
+    transactionalObject.get().insertCountInactive();
     long counter = systemUserDao.get().countInactive();
     assertThat(counter).isEqualTo(1L);
   }
 
-  public static class TransactionalObject {
+  static class TransactionalObject {
 
     @Inject private Provider<EntityManager> entityManager;
 
     @Transactional
-    public void insertInactive() {
+    public void insert() {
+      SystemUser systemUser = new SystemUser();
+      systemUser.setId(222L);
+      systemUser.setEmail("user-222@gmail.com");
+      systemUser.setActive(Boolean.TRUE);
+      entityManager.get().persist(systemUser);
+    }
+
+    @Transactional
+    public void insertCountActive() {
+      SystemUser systemUser = new SystemUser();
+      systemUser.setId(333L);
+      systemUser.setEmail("user-333@gmail.com");
+      systemUser.setActive(Boolean.TRUE);
+      entityManager.get().persist(systemUser);
+    }
+
+    @Transactional
+    public void insertCountInactive() {
       SystemUser systemUser = new SystemUser();
       systemUser.setId(111L);
       systemUser.setEmail("dare@devil.com");
