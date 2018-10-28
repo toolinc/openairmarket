aaa60637bd3b1d92e7b9e311871e9d22266c39d1
encomp <edgar.martinez.rico@gmail.com>
2018-10-28 09:50:33 -0700

    Remove unused codes.

--- java/common/src/main/java/com/openairmarket/common/persistence/dao/DaoErrorCode.java
+++ java/common/src/main/java/com/openairmarket/common/persistence/dao/DaoErrorCode.java
@@ -6,25 +6,11 @@ import javax.inject.Provider;
 /** Defines the error codes for the {@code Dao}. */
 public enum DaoErrorCode implements Provider<ErrorCodeProperty> {
 
-  /** Property that specifies the error in case the primary key passed is not found. */
-  NO_RESULT("dao.noResult"),
-  /** Property that specifies the error in case the persistence layer failed. */
-  PERSISTENCE("dao.persistence"),
   /**
    * Property that specifies the error message in case the entity has been modified recently by
    * another transaction.
    */
   OPRIMISTIC_LOCKING("dao.optimisticLocking"),
-  /**
-   * Property that specifies the error message in case that the primary key queried failed
-   * unexpectedly.
-   */
-  UNEXPECTED("dao.unexpected"),
-  /**
-   * Property that specifies the error in case the unique has been violated for the name field for
-   * an entity that is a catalog.
-   */
-  ACTIVE_STATUS_MISMATCH("dao.active.mismatch"),
   /**
    * Property that specifies the error in case the unique has been violated for the reference id
    * field for an entity that is a catalog.
